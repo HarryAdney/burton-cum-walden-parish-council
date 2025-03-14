544f17a70264159d6c30dae7d4733a4f9be705e5
HarryAdney <dev@harryadney.com>
2025-03-14 18:52:15 +0000

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'West Burton, Yorkshire';
+export const SITE_DESCRIPTION = 'Welcome to West Burton - a beautiful village in the heart of Wensleydale, Yorkshire Dales';
